72446a4c77ed19533993c29a8e037afb6203d0cb
Cezar <fntblk@gmail.com>
2025-07-27 19:37:05 -0300

    hello world

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # CV-generator-webapp
 LaTeX Curriculum Vitae document generator server application, written in Clojure.
+
+## Prerequisites
+* Leiningen 2.0.0 or above
+
+## Running
+To host the server, run the commands:
+```sh
+cd cv-generator/
+lein ring server
+```
---
+++ cv-generator/project.clj
@@ -0,0 +1,12 @@
+(defproject cv-generator "1.0.0"
+  :description "Curriculum Vitae generator webapp"
+  :url "https://github.com/Faint-Black/CV-generator-webapp"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [compojure "1.6.1"]
+                 [ring/ring-defaults "0.3.2"]]
+  :plugins [[lein-ring "0.12.5"]]
+  :ring {:handler cv-generator.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring/ring-mock "0.3.2"]]}})
